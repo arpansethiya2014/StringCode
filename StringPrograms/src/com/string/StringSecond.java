52db90ffbf7a11b82b045e6515357e4e075369e0
arpansethiya78699 <arpansethiya2014@gmail.com>
2020-09-22 23:39:10 +0530

    Reverse String Program

---
+++ StringPrograms/src/com/string/StringSecond.java
@@ -0,0 +1,13 @@
+package com.string;
+
+public class StringSecond {
+  public static void main(String[] args) {
+	String s = "Ram is a student";
+	String s1 = new String();
+	for (int i = s.length()-1; i >= 0; i--) {
+		s1 = s1 + s.charAt(i);
+		
+	}
+	System.out.println(s1);
+}
+}
